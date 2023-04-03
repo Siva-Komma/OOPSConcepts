9725ce73e5bb5da2db53ca5e1d8f951b4379e534
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 20:55:59 +0530

    Simple program on Method overriding and overloading of Polymorphism

--- OOP'sConcepts/Program.cs
+++ OOP'sConcepts/Program.cs
@@ -11,7 +11,7 @@ namespace OOP_sConcepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welome to OOPs Concepts");
-            Console.WriteLine("1.Class and Object\n2.Inheritance\n");
+            Console.WriteLine("1.Class and Object\n2.Inheritance\n3.Polymorphism overriding Overloading\n");
             Console.WriteLine("Choose Your Option");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -25,6 +25,21 @@ namespace OOP_sConcepts
                     //Inheritance
                     VehicleDetails vd = new VehicleDetails();
                     vd.Details();
+                    break;
+                case 3:
+                    //Polymorphism - Overriding
+                    Console.WriteLine("Overriding");
+                    Animal animal = new Animal();
+                    Pig pig = new Pig();    
+                    Dog dog = new Dog();
+                    animal.AnimalSound();
+                    pig.AnimalSound();
+                    dog.AnimalSound();
+                    Console.WriteLine("\nOverloading");
+                    // OverLoading
+                    Calculation.Sum(10, 20);
+                    Calculation.Sum(10.5f, 20);
+                    Calculation.Sum("Siva", 30, 40);
                     break;
             }
             Console.ReadLine();
