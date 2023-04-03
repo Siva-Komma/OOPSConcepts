12fb18ef9047dd258bd72d68ed2833c936c6552e
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 11:29:12 +0530

    Displaying Welcome message of OOPs Concepts

---
+++ OOP'sConcepts/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_sConcepts
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welome to OOPs Concepts");
+            Console.ReadLine();
+        }
+    }
+}
