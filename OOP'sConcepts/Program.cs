232aedbb946fe242d0427cc584b263d60b0ad945
Siva-Komma <kommasivajyothi22@gmail.com>
2023-04-03 11:47:31 +0530

    Simple Program on Class and Object

--- OOP'sConcepts/Program.cs
+++ OOP'sConcepts/Program.cs
@@ -11,6 +11,17 @@ namespace OOP_sConcepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welome to OOPs Concepts");
+            Console.WriteLine("1.Class and Object\n");
+            Console.WriteLine("Choose Your Option");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    //Class and Object Concept
+                    PrintNumbers num = new PrintNumbers();
+                    num.Numbers();
+                    break;
+            }
             Console.ReadLine();
         }
     }
